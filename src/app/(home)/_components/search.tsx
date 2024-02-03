f572e7f91208754589c908e9f21d22306e379c00
Cleverson Vieira <cleveira_cv@msn.com>
2024-02-03 14:15:43 -0300

    feat: add initial search component

---
+++ src/app/(home)/_components/search.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { SearchIcon } from "lucide-react";
+
+const Search = () => {
+  return (
+    <div className="flex items-center gap-2 ">
+      <Input placeholder="Busque por uma barbearia..." />
+      <Button variant={"default"}>
+        <SearchIcon size={20} />
+      </Button>
+    </div>
+  );
+};
+
+export default Search;
