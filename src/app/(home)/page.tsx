53a43d106fc02a1e0a364b797837403214b91533
Cleverson Vieira <cleveira_cv@msn.com>
2024-02-03 14:00:58 -0300

    feat: add welcome message

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,20 @@
+import { format } from "date-fns";
 import Header from "../../components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Cleverson</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM 'de' yyyy", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </div>
   );
 }
