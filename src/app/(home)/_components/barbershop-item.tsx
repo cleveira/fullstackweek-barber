053e589a438931cdfd7c3c506d56b4b5eb1fa711
Cleverson Vieira <cleveira_cv@msn.com>
2024-02-03 15:43:39 -0300

    feat: add initial barbershop item

---
+++ src/app/(home)/_components/barbershop-item.tsx
@@ -0,0 +1,52 @@
+import { Card, CardContent } from "../../../components/ui/card";
+import Image from "next/image";
+import { Barbershop } from "@prisma/client";
+import { Button } from "../../../components/ui/button";
+import { Badge } from "../../../components/ui/badge";
+import { StarIcon } from "lucide-react";
+
+interface BarbershopItemProps {
+  barbershop: Barbershop;
+}
+
+const BarbershopItem = ({ barbershop }: BarbershopItemProps) => {
+  return (
+    <Card className="min-w-[167px] max-w-[167px] rounded-2xl">
+      <CardContent className="px-1 py-0">
+        <div className="w-full h-[159px] relative">
+          <div className="absolute top-2 left-2 z-50">
+            <Badge
+              variant={"secondary"}
+              className="opacity-90 flex gap-1 items-center top-3 left-3"
+            >
+              <StarIcon size={12} className="fill-primary text-primary" />
+              <span>5,0</span>
+            </Badge>
+          </div>
+          <Image
+            src={barbershop.imageUrl}
+            alt={barbershop.name}
+            style={{
+              objectFit: "cover",
+            }}
+            fill
+            className="rounded-2xl"
+          />
+        </div>
+        <div className="px-2 pb-3">
+          <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">
+            {barbershop.name}
+          </h2>
+          <p className="text-sm text-gray-400 overflow-hidden text-ellipsis text-nowrap">
+            {barbershop.address}
+          </p>
+          <Button variant={"secondary"} className="w-full mt-3">
+            Reservar
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default BarbershopItem;
